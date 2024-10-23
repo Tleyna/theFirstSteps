815be2ada1b05f2bc7a9f85055c3d0b8df519351
Tleyna <101tleyna@gmail.com>
2024-10-23 20:15:02 +0300

    update

---
+++ factorial2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void)
+{
+  int number;
+
+  printf("Number: ");
+  scanf("%d", &number);
+
+  printf("Факториал %d = %d\n", number, factorial(number));
+}
+
+int factorial(int n)
+{
+  int result = n;
+  for (int i = n - 1; i > 1; i--)
+  {
+    result = result * i;
+  }
+  return result;
+}
